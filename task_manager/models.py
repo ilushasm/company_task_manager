9b996f79ddae0262cdab36929dc07357d6d5d451
Illia Smyrnov <ilushasm@gmail.com>
2023-05-29 00:00:48 +0300

    Add all models in task_manager app

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,64 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=63)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=63)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["username"]
+        verbose_name = "worker"
+        verbose_name_plural = "workers"
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name} ({self.username})"
+
+
+class Task(models.Model):
+    class Priority(models.IntegerChoices):
+        URGENT = 1
+        HIGH = 2
+        NORMAL = 3
+        LOW = 4
+
+    name = models.CharField(max_length=63)
+    description = models.TextField(blank=True)
+    deadline = models.DateField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.IntegerField(choices=Priority.choices)
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(get_user_model(), related_name="assigned")
+
+
+class Team(models.Model):
+    team_lead = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    name = models.CharField(max_length=63)
+    members = models.ManyToManyField(get_user_model(), related_name="teammates")
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=63)
+    description = models.TextField(blank=True)
+    tasks = models.ManyToManyField(Task, related_name="project")
+    team = models.ManyToManyField(Team, related_name="project")
