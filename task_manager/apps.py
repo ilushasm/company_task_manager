128a2b66e4af66d7fc02793ef1a2dc3bc8706f9a
Illia Smyrnov <ilushasm@gmail.com>
2023-06-06 21:50:04 +0300

    Moved create_groups_and_permissions from utils.py to a migration 0013

--- task_manager/apps.py
+++ task_manager/apps.py
@@ -5,7 +5,3 @@ class TaskManagerConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "task_manager"
 
-    def ready(self):
-        from .utils import create_groups_and_permissions
-
-        create_groups_and_permissions()
