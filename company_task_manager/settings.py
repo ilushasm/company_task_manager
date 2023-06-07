8ded1753c7994edb26b33264f0b5fd5138d5ecd8
Illia Smyrnov <ilushasm@gmail.com>
2023-06-07 15:03:24 +0300

    Decalred secret_ket as environment variable

--- company_task_manager/settings.py
+++ company_task_manager/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-#-fp#&=^g6wvs1x6re%grosj&9zek7c33@bv-@qv4wsv38j8bs"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
